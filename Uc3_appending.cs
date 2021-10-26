cb9f13f58a68380efbc57ae598d8d9d6a66726d2
faheemjailor123 <93036039+faheemjailor123@users.noreply.github.com>
2021-10-26 08:36:51 -0700

    UC3_appending.cs

---
+++ Uc3_appending.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace LinkedList01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
+            list.Display();
+
+        }
+    }
+}
