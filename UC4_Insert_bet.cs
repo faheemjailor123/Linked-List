ddf61ffdc7df86dfb676c64ca7412c1f10b8419f
faheemjailor123 <93036039+faheemjailor123@users.noreply.github.com>
2021-10-26 08:38:50 -0700

    UC4_inser

---
+++ UC4_Insert_bet.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace LinkedList01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Append(56);
+            list.Append(70);
+            list.Display();
+            Console.WriteLine();
+            list.InsertAt(2, 30);
+            list.Display();
+
+        }
+    }
+}
